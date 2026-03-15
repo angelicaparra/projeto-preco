0c9c30a798d6a08ad8f02d1b6ba6e4b2e77b3a43
angelicaparra <angelicaparraa@gmail.com>
2026-03-15 19:38:21 -0300

    Alterando o processor para a limpagem dos dados

--- processor.py
+++ processor.py
@@ -1,28 +1,50 @@
 import pandas as pd
 import os
+import io
 
 def processar_dados():
-    caminho_arquivo = 'data/boi_gordo.csv'
+    origem_arquivo = 'data'
+    destino_arquivo = 'data/limpos'
 
-    if not os.path.exists(caminho_arquivo):
-        print("❌ Arquivo CSV não encontrado. Por favor, execute o scraper primeiro.")
-        return
+    if not os.path.exists(destino_arquivo):
+        os.makedirs(destino_arquivo)
 
-    df = pd.read_csv(caminho_arquivo)
-    df.columns = ['data', 'preco_reais', 'var_dia', 'var_mes', 'preco_dolar']
+    arquivos = [f for f in os.listdir(origem_arquivo) if f.endswith('.csv') and 'limpo' not in f]
+    print(f"🧹 Iniciando limpeza de {len(arquivos)} arquivos...")
 
-    # Nota: O CEPEA às vezes traz o valor multiplicado por 100 ou com vírgula.
-    # Tratando para que seja um número real.
-    df['preco_reais'] = df['preco_reais'].astype(float) / 100
+    for arquivo in arquivos:
+        try:
+            caminho_input = os.path.join(origem_arquivo, arquivo)
+            df = pd.read_csv(caminho_input)
 
-    #convertendo a coluna de data para o formato datetime
-    df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y')
+            # Nota: O CEPEA às vezes traz o valor multiplicado por 100 ou com vírgula.
+            # Tratando para que seja um número real.
+            colunas_originais = df.columns.tolist()
+            df = df.rename(columns={
+                colunas_originais[0]: 'data',
+                colunas_originais[1]: 'preco_reais'
+            })
+            #convertendo a coluna de data
+            df['data'] = pd.to_datetime(df['data'], format='%d/%m/%Y', errors='coerce')
 
-    # Salvando o arquivo limpo
-    df.to_csv('data/boi_gordo_limpo.csv', index=False, encoding='utf-8-sig')
+            # Removendo pontos de milar e trocar vírgula por ponto 
+            if df['preco_reais'].dtype == object:
+                df['preco_reais'] = df['preco_reais'].str.replace('.', '', regex=False).str.replace(',', '.', regex=False)
+            
+            df['preco_reais'] = pd.to_numeric(df['preco_reais'], errors='coerce')
 
-    print("✨ Dados processados e salvos em 'data/boi_gordo_limpo.csv'!")
-    print(df.head())
+            # Garantinhdo que não haja valores nulos
+            df = df.dropna(subset=['data', 'preco_reais'])
+
+            nome_limpo = arquivo.replace('.csv', '_limpo.csv')
+            df[['data', 'preco_reais']].to_csv(os.path.join(destino_arquivo, nome_limpo), index=False, encoding='utf-8-sig')
+
+            print(f"✅ {arquivo} processado!")
+
+        except Exception as e:
+            print(f"⚠️ Erro ao processar {arquivo}: {e}")      
+
+    print(f"\n✨ Tudo pronto! Arquivos limpos salvos em: {destino_arquivo}")  
 
 if __name__ == "__main__":
     processar_dados()
